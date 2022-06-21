64d2395f138ed71bf98a6b4a85909b7ee5939c1f
diy0r4ik <95129772+diy0r4ik@users.noreply.github.com>
2022-06-21 23:06:05 +0500

    adca

---
+++ js/diyor.js
@@ -0,0 +1,92 @@
+// let wrapper = document.querySelector(".wrapper");
+
+
+// // fetch("https://jsonplaceholder.typicode.com/users")
+// // .then((res) => res.json())
+// // .then((users) =>  console.log(users))
+
+
+// fetch("#A")
+// .then((res) => res.json())
+// .then((A) => console.log(A),
+
+//         A.Search.forEach(element => {
+//             console.log(element);
+//             wrapper.innerHTML +=`
+//             <div class="card">
+//             <div class="img">
+//             <p>${element.name}</p>
+//             </div>
+//                 <h2>${element.Title}</h2>
+//                 <p>${element.Year}</p>
+//             </div>
+//             `
+//             ;
+//         })
+// );
+for(var i=1;i<=Math.ceil(A.length/9);i++){
+document.querySelector('.pages11').innerHTML +=`
+<li class="page-item"><a onclick='my(${i})' class="page-link" href="#">${i}</a></li>`
+}
+ var e=1
+plus12=()=>{
+if(Math.ceil(A.length/9)>=e+1){
+  e++
+}
+my(e)
+}
+
+minus12=()=>{
+  if(0<e-1){
+  e--
+  } 
+  my(e)
+}
+
+function my(x){
+    document.querySelector('.cards12').innerHTML=" " 
+    for(var j=x*9-9;j<x*9;j++){
+        document.querySelector('.cards12').innerHTML+= ` 
+        <div class="card">
+        <div class="item-1">
+            <a href="#"><img style="min-height:285px" src="${A[j].img}" alt="Image2" class="img-fluid img-fluid1"></a>
+            <div class="item-1-contents">
+              <div class="text-center">
+              <h3><a href="#">${A[j].name}</a></h3>
+              <div class="rating">
+                <span class="icon-star text-warning"></span>
+                <span class="icon-star text-warning"></span>
+                <span class="icon-star text-warning"></span>
+                <span class="icon-star text-warning"></span>
+                <span class="icon-star text-warning"></span>
+              </div>
+              <div class="rent-price"><span>${A[j].sena}</span></div>
+              </div>
+              <ul class="specs">
+                <li>
+                  <span>Расход:</span>
+                  <span class="spec">${A[j].door}</span>
+                </li>
+                <li>
+                  <span>Пробег:</span>
+                  <span class="spec">${A[j].seat}</span>
+                </li>
+                <li>
+                  <span>Объём двигателя</span>
+                  <span class="spec">${A[j].transmision}</span>
+                </li>
+                <li>
+                  <span>Выпуск</span>
+                  <span class="spec">${A[j].age}</span>
+                </li>
+              </ul>
+              <div class="d-flex action">
+                <a href="contact.html" class="btn btn-primary">Арендовать сейчас</a>
+              </div>
+            </div>
+          </div>
+      </div>
+        `
+    }
+}
+my(1)
---
+++ js/json.js
@@ -0,0 +1,35 @@
+var A = [
+    {
+        id: 1,
+        name:'Mercedes Benz CLA 200',
+        img:'https://avatars.mds.yandex.net/get-autoru-vos/4551970/691aa1bed3d9e7c0ce58ff9d680aa356/456x342n',
+        rate: 5,
+        sena:'$62.000',
+        door: '5.7 л',
+        seat: '39.896 км',
+        transmision:'1.3 л Турбо',
+        age: 2019
+    },
+    {
+        id:2,
+        name:'Hyundai Sonata',
+        img:'https://cache3.pakwheels.com/ad_pictures/6322/hyundai-sonata-2-5-2-2022-63221330.jpg',
+        rate: 5,
+        sena:'$37.000',
+        door:"2.5 л",
+        seat:"48.838 км",
+        transmision:'180 л.с',
+        age:2020
+    },
+    {
+        id:3,
+        name:'TOYOTA VENZA HYBRID',
+        img:'https://pictures.dealer.com/h/hometowntoyota/0441/f7c570ccf912e85014a8c43257833688x.jpg?impolicy=downsize&w=568',
+        rate: 5,
+        sena:'$79.000',
+        door:"6л",
+        seat:"Без пробег",
+        transmision:'2.5л',
+        age:2022
+    }
+]
